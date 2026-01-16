b8144bfda7a426a25fac0e585de77e7868827cc2
Rod <rodrigo.godoy93@hotmail.com>
2026-01-16 13:00:02 +0100

    fixed 2.0

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  // 1. ¿Tienes esto aquí arriba?
+  reporter: 'cypress-mochawesome-reporter',
+  
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // 2. ¡ESTA ES LA LÍNEA MÁGICA QUE SUELE FALTAR!
+      require('cypress-mochawesome-reporter/plugin')(on);
+
+      return config;
     },
   },
 });
